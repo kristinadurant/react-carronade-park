eb81449793eabed85c20d31b04cfb319abd8b804
kristinadurant <kristinadurant.p@gmail.com>
2021-02-03 19:50:47 -0500

    added lightbox to floor plans

--- src/components/FloorPlan.jsx
+++ src/components/FloorPlan.jsx
@@ -1,37 +1,26 @@
 import React, { useContext } from 'react';
 import { ModalContext } from '../context/ModalContext';
 import { SRLWrapper } from "simple-react-lightbox";
+import { useLightbox } from 'simple-react-lightbox'
 
 const FloorPlan = ({ images }) => {
-    const { setModal} = useContext(ModalContext);
+    const { openLightbox } = useLightbox();
 
     return (
         <>
-        <SRLWrapper>
-            {images.map((item, index) => (
-                <a href={item} key={index}>
-                    <img src={item} alt="text" />
-                </a>
-            ))}
-        </SRLWrapper>
-        {/* <div className="img-wrapper">
-            {images.map((item, index) => (
-                <img 
-                    key={index}
-                    src={item}
-                    alt='floor plan'
-                    onClick={() => setModal([item, 'floor plan'])}
-                />
-            ))}
-        </div>
-        <p className='details'>
-            <span>3 BED</span>
-            <span>2.5 BATH</span>
-            <span>700 SF</span>
-        </p>
-        <a className='button' href='#' target='_blank' rel='noreferrer'>
+            <div>
+                {images.map((item) => (
+                    <div key={item.id}>
+                        <img src={item.url} alt={item.alt} onClick={() => openLightbox(item.id)} srl_elementid={item.id}/>
+                    </div>
+                ))}
+            </div>
+            <p className='details'>
+                <span>3 BED</span><span>2.5 BATH</span><span>700 SF</span>
+            </p>
+            <a className='button' href='#' target='_blank' rel='noreferrer'>
                 DOWNLOAD
-        </a> */}
+            </a>
         </>
     )
 }
--- src/pages/FloorPlans.jsx
+++ src/pages/FloorPlans.jsx
@@ -1,33 +1,70 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import FloorPlan from '../components/FloorPlan';
+import { SRLWrapper } from "simple-react-lightbox";
 
 const FloorPlans = () => {
-
+    const options = {
+        settings: {
+            disableKeyboardControls: true,
+            disableWheelControls: true
+        },
+        thumbnails: {
+            showThumbnails: false
+        },
+        buttons: {
+            showAutoplayButton: false,
+            showFullscreenButton: false,
+            showNextButton: false,
+            showPrevButton: false,
+            showThumbnailsButton: false
+        }
+    }
     const images1 = [
-        'https://res.cloudinary.com/carronadepark/image/upload/v1611351497/Floor%20Plans/first_floor_1_gaw1v5.jpg'
+        { 
+            id: 0,
+            url: 'https://res.cloudinary.com/carronadepark/image/upload/v1611351497/Floor%20Plans/first_floor_1_gaw1v5.jpg',
+            alt: 'One Floor Home'
+        }
     ];
     const images2 = [
-        'https://res.cloudinary.com/carronadepark/image/upload/v1611351497/Floor%20Plans/first_floor_1_gaw1v5.jpg',
-        'https://res.cloudinary.com/carronadepark/image/upload/v1611351495/Floor%20Plans/second_floor_1_rachlw.jpg'
+        {
+            id: 1,
+            url: 'https://res.cloudinary.com/carronadepark/image/upload/v1611351497/Floor%20Plans/first_floor_1_gaw1v5.jpg',
+            alt: 'First Floor'
+        },
+        {
+            id: 2,
+            url: 'https://res.cloudinary.com/carronadepark/image/upload/v1611351495/Floor%20Plans/second_floor_1_rachlw.jpg',
+            alt: 'Second Floor'
+        }
     ]
 
     return (
         <div id="floor-plans-page">
             <Banner text={'Floor Plans'} image={'https://res.cloudinary.com/carronadepark/image/upload/v1611285585/Gallery/IMG_1716_Large_s75upg.jpg'}/>
             <section className="inner intro">
-            <p>Floorplans are available for two groups of apartments, Fort Meigs at Levis and Williamsburg West. 
-            Below please find the floor plans for each of various apartment homes.</p>
+                <p>Floorplans are available for two groups of apartments, Fort Meigs at Levis and Williamsburg West. 
+                Below please find the floor plans for each of various apartment homes.</p>
             </section>
             <section id='floor-plans-blocks' className="inner">
-                <div>
+                <div className="container">
                     <h2>One Floor Homes</h2>
-                    <FloorPlan images={images1} />
+                    <FloorPlan images={images1} />         
                 </div>
-                <div>
+                <div className="container">
                     <h2>Two Floors Homes</h2>                
                     <FloorPlan images={images2} />
                 </div>
+            </section>
+            <section id="floor-plans-modal" style={{display: 'none'}}>
+                <SRLWrapper options={options}>
+                {[...images1, ...images2].map((item) => (
+                    <a href={item.url} key={item.id}>
+                        <img src={item.url} alt={item.alt} />
+                    </a>
+                ))}
+                </SRLWrapper>
             </section>
         </div>
     )
